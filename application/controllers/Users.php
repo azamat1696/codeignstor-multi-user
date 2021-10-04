f8b16a21da4220790378da3983039d0540c61ce5
azamat1696 <azamat1696@gmail.com>
2021-10-04 08:58:03 +0300

    framework ayarları yapıldı

---
+++ application/controllers/Users.php
@@ -0,0 +1,10 @@
+<?php
+
+class Users extends CI_Controller {
+   public function __construct() {
+       parent::__construct();
+   }
+   public function index() {
+       echo "Welcome bro";
+   }
+}
